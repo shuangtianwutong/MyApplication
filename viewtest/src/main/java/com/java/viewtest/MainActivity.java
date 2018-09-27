f4faf3c2fdad73a7868e2e86d75bc62f58fb4329
zhangrongzhao <zhangrongzhao@didichuxing.com>
2018-09-27 19:14:13 +0800

    测试git

--- viewtest/src/main/java/com/java/viewtest/MainActivity.java
+++ viewtest/src/main/java/com/java/viewtest/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test developer1
     }
 
     public void  onButtonClickJsonTest(View view) {
