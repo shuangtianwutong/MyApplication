6a2e92aabbc61d76eac26cfa65de526e3d93a47c
zhangrongzhao <zhang_rongzhao@sina.com>
2018-09-27 19:14:13 +0800

    test

--- jsonteststudy/src/main/java/com/study/schemeurl/Scheme1Activity.java
+++ jsonteststudy/src/main/java/com/study/schemeurl/Scheme1Activity.java
@@ -2,6 +2,7 @@ package com.study.schemeurl;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,10 +10,12 @@ import android.widget.TextView;
 
 import com.study.jsontest.R;
 
+import java.net.URLDecoder;
+
 public class Scheme1Activity extends AppCompatActivity {
     public static final String WEBAPP_TRAFIC_REPORT_URL = "https://poi.map.xiaojukeji.com/sugreport";//h5方案用来填写sug页新增上报的url
-    public static final String WEBAPP_TRAFIC_REPORT_ADD_PATH = "new";//h5方案用来填写sug页新增上报的Uri的path部分
-    public static final String WEBAPP_TRAFIC_REPORT_WRONG_PATH = "sugmodify";//h5方案用来填写sug页报错的的url的path部分
+    public static final String WEBAPP_TRAFIC_REPORT_ADD_FRAGMENT = "/new";//h5方案用来填写sug页新增上报的Uri的Fragement部分
+    public static final String WEBAPP_TRAFIC_REPORT_WRONG_FRAGMENT = "/modify";//h5方案用来填写sug页报错的的url的Fragement部分
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +27,23 @@ public class Scheme1Activity extends AppCompatActivity {
     }
 
     public void getUrlFromParmeter(){
-        Uri.Builder mBuilder = Uri.parse(WEBAPP_TRAFIC_REPORT_URL ).buildUpon();
-        mBuilder = mBuilder.appendPath(WEBAPP_TRAFIC_REPORT_ADD_PATH);
+        Uri.Builder mBuilder = Uri.parse(WEBAPP_TRAFIC_REPORT_URL).buildUpon();
+        //mBuilder =mBuilder.appendEncodedPath("");
+        //Log.d("zrz2018","1-mBuilder.build()="+mBuilder.build().toString());
+        //mBuilder = mBuilder.appendEncodedPath(WEBAPP_TRAFIC_REPORT_ADD_PATH);
         mBuilder = mBuilder.appendQueryParameter("test","1223");
         mBuilder = mBuilder.appendQueryParameter("id","4556");
-
+        mBuilder = mBuilder.encodedFragment(WEBAPP_TRAFIC_REPORT_ADD_FRAGMENT);
+       // urlStr  =urlStr.contains("#")?urlStr.replace("#", "%23"):urlStr;
+       // urlStr  =urlStr.contains("%23")?urlStr.replace("%23", "#"):urlStr;
+       /* String urlStr = mBuilder.build().toString() ;
+        urlStr = urlStr+"#new";
+        Uri uri =Uri.parse(urlStr);*/
 
         textView.setText(mBuilder.build().toString());
-        Log.d("zrz2018","mBuilder.build()="+mBuilder.build().toString());
+        Log.d("zrz2018","2-mBuilder.build()="+mBuilder.build().toString());
+       // Log.d("zrz2018","4-mBuilder.build()="+uri);
+
     }
 
     public void parseSchemUrl(Intent intent){
@@ -64,4 +76,5 @@ public class Scheme1Activity extends AppCompatActivity {
         System.out.println(" uri2:" +  UriUtils.getInstance().getUri());
 
     }
+
 }
--- viewtest/src/main/java/com/java/viewtest/MainActivity.java
+++ viewtest/src/main/java/com/java/viewtest/MainActivity.java
@@ -2,19 +2,24 @@ package com.java.viewtest;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends AppCompatActivity {
-
+   private ViewGroup mlinerLayoutTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mlinerLayoutTest = (LinearLayout)findViewById(R.id.linerlayouttest);
         //test developer1
     }
 
@@ -32,8 +37,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void  onButtonClickSchemrTest(View view) {
 
+
         startActivity(new Intent(this.getApplicationContext(),ScrollerTestActivity.class));
         //     //test developer1 test2
+
+    }
+
+    public void onShapeTest(View view) {
+        GradientDrawable p = (GradientDrawable) mlinerLayoutTest.getBackground();
+        p.setColor(Color.parseColor("#ebebeb"));
 
     }
 
